78f4e0bbe0493c3689b389c2ca334e3027566632
Zomb13 <166577207+Zomb1342@users.noreply.github.com>
2025-07-25 18:57:34 -0500

    Update pcileech_com.sv

--- src/pcileech_com.sv
+++ src/pcileech_com.sv
@@ -69,8 +69,11 @@ module pcileech_com (
             // please consult sources and also device_fpga.c in the LeechCore project.
             64'h00000000_00000000,
             64'h00000000_00000000,
-            64'h00000000_00000000,
-            64'h00000000_00000000,
+            // Set x1 width (addr 0x061, data 0x01 for bits [5:0])
+            64'h0100_3F00_0061_2377,
+        
+            // Set Gen1 speed (addr 0x025, data 0x10 for bits [13:10])
+            64'h0010_00F0_0025_2377,
 
             // Bring the PCIe core online from initial hot-reset state. This is done by
             // setting control bit in PCIleech FIFO CMD register. This should ideally be
